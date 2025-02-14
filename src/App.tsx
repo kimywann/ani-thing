9c94d3541481a3aafba3347f1a8aa59f8a2dba92
kimywann <kimywan10@gmail.com>
2025-02-14 22:35:14 +0900

    feat: 라우터 설정 및 레이아웃 컴포넌트 적용, 오류 처리 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { GlobalStyle } from './components/GlobalStyle';
+import Home from './pages/Home';
+import Layout from './layout/Layout';
+import RouterError from './components/common/RouterError';
+
+const routeList = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/discussion',
+    element: <div>애니 토론 & 투표</div>,
+  },
+  {
+    path: '/community',
+    element: <div>커뮤니티</div>,
+  },
+];
+
+const router = createBrowserRouter(
+  routeList.map((item) => {
+    return {
+      ...item,
+      element: <Layout>{item.element}</Layout>,
+      errorElement: <RouterError />,
+    };
+  }),
+);
+
 function App() {
-  return <div>AniThing</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
