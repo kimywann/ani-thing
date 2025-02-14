507748b09bb46f303abe10104ae76ae0b035f180
kimywann <kimywan10@gmail.com>
2025-02-14 22:33:06 +0900

    feat: 홈 페이지 컴포넌트 추가, 사이드바와 헤더에 맞춘 레이아웃 설정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  margin-left: 200px; /* 사이드바 너비만큼 여백 */
+  margin-top: 40px; /* 헤더 높이만큼 여백 */
+  display: flex;
+  justify-content: center; /* 가로 가운데 정렬 */
+  align-items: center;
+  height: calc(90vh - 50px);
+  background-color: blue;
+`;
+
+const Home = () => {
+  return <HomeContainer>Home</HomeContainer>;
+};
+export default Home;
